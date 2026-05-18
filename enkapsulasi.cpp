e69d8a41ce97a8f0f587887cd14f53eced506e83
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-18 17:51:14 +0700

    object remotlampu

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,12 @@ public:
         return saklarNo[i];
     }
 };
+
+int main() {
+    remotLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+}
