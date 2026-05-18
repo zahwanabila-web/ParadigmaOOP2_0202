83b5d21b526bd40b7b0f7a7181f61c535fb13fe6
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-18 17:26:49 +0700

    class seseorang

---
+++ polimorpi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+    // virtual void pesan(){
+    //     cout<<"pesan dari seseorang "<<endl;
+    // }
+};
