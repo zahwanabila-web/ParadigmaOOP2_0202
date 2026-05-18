078741da91827f256d7daf4b4ee1fb40138ab521
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-18 18:37:30 +0700

    solvemultiwaris

---
+++ solveMultiwaris.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    orang(int pUmur) :
+    umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+class pekerja : public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja di buat\n" << endl;
+    }
+};
+
+class pelajar : public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+
+};
