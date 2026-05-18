5cfda993af09feba5e3d425ed2070fe521b6cb6a
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-18 18:18:54 +0700

    class pekerja

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -10,3 +10,39 @@ public:
             cout << "orang di buat dengan umur " << umur << "\n" <<endl;
         }
 };
+
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur) :
+    orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+public:
+    
+    budi(int pUmur) :
+       pekerja(pUmur),
+       pelajar(pUmur),
+       orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+       {
+        cout << "Budi dibuat\n" << endl;
+       }
+};
+
+int main() {
+    budi a(12);
+
+    return 0;
+}
